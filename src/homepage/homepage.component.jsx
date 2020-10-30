3c111952b9de50733df1b23c6b35c636a9679bf0
Mahamadou Djeidy Ba <mdjeidyba@atlassian.com>
2020-10-30 16:58:39 -0700

    add homepage styles

--- src/homepage/homepage.component.jsx
+++ src/homepage/homepage.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './homepage.styles.scss';
 
 const Homepage = () => {
   return (
     <div className={'homepage'}>
-      <div className={'directory-meny'}>
+      <div className={'directory-menu'}>
         <div className={'menu-item'}>
           <div className="content">
             <h1 className="title">HATS</h1>
